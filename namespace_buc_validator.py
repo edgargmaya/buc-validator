5fc81763594fc86db97de29c43418a0b3363e833
edgargmaya <32275463+edgargmaya@users.noreply.github.com>
2025-08-12 01:00:06 -0600

    Update namespace_buc_validator.py

--- namespace_buc_validator.py
+++ namespace_buc_validator.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import re
-import json
-from typing import Iterable, List, Dict, Any, Sequence
+from typing import List, Dict, Any, Sequence
 from kubernetes import client, config
 
 PATTERN_1 = re.compile(r'^\d{4}\.\d{6}\.\d{4}\.\d{6}\.\d{4}\.C139\.\d{4}$')
@@ -26,7 +25,6 @@ def collect_patterns(ns: Dict[str, Any] | None = None) -> Sequence[re.Pattern]:
     return patterns
 
 def evaluate_namespaces(
-    ns_items: Iterable[Any],
     patterns: Sequence[re.Pattern] | None = None,
     name_substring: str = "lob",
     label_key: str = "buc",
@@ -34,6 +32,10 @@ def evaluate_namespaces(
     """
     Receives an iterable list of objects with .metadata.name and .metadata.labels, and returns the non compliance list.
     """
+    config.load_kube_config()
+    v1 = client.CoreV1Api()
+    ns_items = v1.list_namespace().items
+
     patterns = tuple(patterns or collect_patterns())
     lob_ns = [ns for ns in ns_items if name_substring in ns.metadata.name]
 
@@ -46,16 +48,8 @@ def evaluate_namespaces(
         if buc and not any(p.match(buc) for p in patterns):
                 namespaces_with_invalid_buc.append(ns.metadata.name)
 
-    return {
-        "namespaces_with_invalid_buc": namespaces_with_invalid_buc,
-    }
+    return namespaces_with_invalid_buc
 
-def main() -> None:
-    config.load_kube_config()
-    v1 = client.CoreV1Api()
-    ns_items = v1.list_namespace().items
-    result = evaluate_namespaces(ns_items)
-    print(json.dumps(result, indent=2, ensure_ascii=False))
 
-if __name__ == "__main__":
-    main()
+
+print(evaluate_namespaces())
